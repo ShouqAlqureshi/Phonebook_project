eb7d02fb7b9c58d981a1e0f491e757b4a48f417f
ShouqAlqureshi <123020118+ShouqAlqureshi@users.noreply.github.com>
2023-10-04 23:08:48 +0300

    finished insertToSortedList and add methods they are both done&tested

--- src/ContactLinkedListADT.java
+++ src/ContactLinkedListADT.java
@@ -6,26 +6,17 @@ public class ContactLinkedListADT{
         this.current = null;
     }
 
-    public void add(Contact contactToInsert){//tested &done but names sorting is a prob
+    public void add(Contact contactToInsert){//tested &done
         if(isUniqueContact(contactToInsert)) {
             if(this.head == null) {
-                head = new Node<Contact>(contactToInsert);
-                current = head;
-                sort(this);
+                insertToSortedList(new Node<Contact>(contactToInsert) );
                 System.out.println(contactToInsert.toString()+"\n##has been added to the phonebook successfully ;)");
                 return;
+            } if (current.next==null){// current will be last element,and it's next is null (last in first out)
+                insertToSortedList(new Node<Contact>(contactToInsert) );
+            } else if (hasNext()) {//current will be the new element ,and it will be in the middle of the list
+                insertToSortedList(new Node<Contact>(contactToInsert) );
             }
-            if (current.next==null){// current will be last element,and it's next is null (last in first out)
-                current.next = new Node<Contact> (contactToInsert);
-                current=current.next;
-            } else if (hasNext()) {//current will be the new element ,and it will be in the middle of the list,this part will be reconsidered
-               Node<Contact> temp = current.next;
-               current.next = new Node<Contact> (contactToInsert);
-               current = current.next;
-               current.next = temp;
-            }
-
-            sort(this);//needs analyzing sort algo
 
             System.out.println(contactToInsert.toString()+"\n##has been added to the phonebook successfully ;)");
             return;
@@ -33,8 +24,8 @@ public class ContactLinkedListADT{
         System.out.println(contactToInsert.toString()+"\n is already added to the phonebook :) ");
     }
 
-    public void remove (Node<Contact>item) {//remove at current which is item//not done yet
-        current=item;
+    public void remove (Node<Contact> contactToRemove) {//remove at current which is item//not done yet
+        current = contactToRemove;
         if (current == head) {
             head = head.next;
         }
@@ -86,7 +77,7 @@ public class ContactLinkedListADT{
         return current.next != null;
     }
 
-   /* public void sort(ContactLinkedListADT list) {//not done yet
+   /* public void sort(ContactLinkedListADT list) {//not done yet  didn't work
         Contact temp;
         if ( list.head.next == null){
             System.out.println("**only one element in list**");//for testing
@@ -104,9 +95,9 @@ public class ContactLinkedListADT{
             }
 
         }
-    }*/
+    }
 
-    public void sort(ContactLinkedListADT list) {//not done yet
+    public void sort(ContactLinkedListADT list) {//not done yet didn't work
         ContactLinkedListADT sortedList= new ContactLinkedListADT();
         sortedList.current= sortedList.head;
         Node<Contact> temp;
@@ -116,8 +107,8 @@ public class ContactLinkedListADT{
             insertToSortedList(list.current,sortedList);
             list.current = temp;
         }
-        list=sortedList;
-//        list.current=sortedList.current;//this=sortedlist
+        this.head=sortedList.head;
+        this.current=sortedList.current;
 
     }
     public void insertToSortedList(Node<Contact> unsortedElement,ContactLinkedListADT sortedList ){
@@ -129,15 +120,15 @@ public class ContactLinkedListADT{
                 sortedList.head = unsortedElement;
                 sortedList.current = sortedList.head;
             } else if (sortedList.head != null)  {//assigning prob in pre
-//                temp= sortedList.head;
-//                while(temp.next != current){
-//                    if (temp.next == null)
-//                        break;
-//                    temp=temp.next;
-//                }
+                temp= sortedList.head;
+                while(temp.next != sortedList.current){
+                    if (temp.next == null)
+                        break;
+                    temp=temp.next;
+                }
                 unsortedElement.next = sortedList.current;// bigger will be added before current of sorted list (changing the link)
                 sortedList.current = unsortedElement;//(adding to the sortedlist)
-//                temp.next=unsortedElement;
+                temp.next=unsortedElement;
             }
         } else {
             Node<Contact> tempCurrent = sortedList.current;//عشان ما احوس اللست القديمه
@@ -149,7 +140,29 @@ public class ContactLinkedListADT{
             sortedList.current.next = unsortedElement;//add as last element
             sortedList.current=unsortedElement;//move current to last element
         }
+        this.head=sortedList.head;
+        this.current=sortedList.current;
     }
+*/
+
+    public void insertToSortedList(Node<Contact> unsortedElement){//last case where 2 is output
+        if (head == null || head.data.compareTo(unsortedElement.data) == 1 || head.data.compareTo(unsortedElement.data) == 0) {//head.data >= unsortedElement.data
+            unsortedElement.next = head;
+            current = head;
+            head = unsortedElement;
+        }
+        else {
+            current = head;
+            while (current.next != null && current.next.data.compareTo(unsortedElement.data) == -1 ){//current.next.data < unsortedElement.data
+                current = current.next;//to check sorting
+            }//trick to assign after looping
+            unsortedElement.next = current.next;
+            current.next = unsortedElement;
+            current = current.next;
+        }
+    }
+
+
 
     public boolean isUniqueContact(Contact contact){//current position after method: current will be the last element+tested
         if (this.head==null){
@@ -181,15 +194,15 @@ public class ContactLinkedListADT{
     public static void main(String[] args) {//testing area
         Contact c1 = new Contact("Ahmad AlSaud", "05937636532", "jnjnjnj", "nnk222", "299","299");
         Contact c2 = new Contact("Ahmad Alzaid", "7968867456", "njnjnj", "mmkmkm", "klll","299");
-        Contact c3 = new Contact("b2", "7968867456", "njnjnj", "mmkmkm", "klll","299");
-        Contact c4 = new Contact("b1", "7968867456", "njnjnj", "mmkmkm", "klll","299");
+        Contact c3 = new Contact("Ahmad", "7968867456", "njnjnj", "mmkmkm", "klll","299");
+        Contact c4 = new Contact("b2", "7968867456", "njnjnj", "mmkmkm", "klll","299");
         Contact c5 = new Contact("b6", "7968867456", "njnjnj", "mmkmkm", "klll","299");
         Contact c6 = new Contact("b4", "7968867456", "njnjnj", "mmkmkm", "klll","299");
         Contact c7 = new Contact("b3", "7968867456", "njnjnj", "mmkmkm", "klll","299");
 
         ContactLinkedListADT cll=new ContactLinkedListADT();
-        cll.add(c1);//added successfully
-        cll.add(c2);// exist not added /first element checked
+        cll.add(c1);
+        cll.add(c2);
         cll.add(c3);
         cll.add(c4);
         cll.add(c5);
