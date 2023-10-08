efd9009389da7f83e78b850848f83340a0d7ad86
AleenM <145330334+AleenM@users.noreply.github.com>
2023-10-08 21:20:51 +0300

    Update Event.java
    calculating big o
    we have to calculate it for set contact + to string since they are comments
    and for compare to since it's complex

--- src/Event.java
+++ src/Event.java
@@ -10,60 +10,70 @@ public class Event /*implements Comparable<Event> */ {
  
 
     public Event() {
-        this.title = "";
-        this.date = null;
-        this.time = "";
-        this.location = "";
-//        this.contact = new Contact();
+        this.title = ""; //1
+        this.date = null; //1
+        this.time = ""; //1
+        this.location = ""; //1
+//        this.contact = new Contact();  1
+        //5 >o(1)
     }
    
     public Event(String title, String date, String time, String location, Contact contact) {
-        this.title = title;
-        this.date = date;
-        this.time = time;
-        this.location = location;
-        this.contact = contact ;
-       
+        this.title = title; //1
+        this.date = date; //1
+        this.time = time; //1
+        this.location = location; //1
+        this.contact = contact ; //1
+       // 5 > o(1)
     }
 
     public void setTitle(String title) {
-        this.title = title;
+        this.title = title; //1
+        // o(1)
     }
 
     public void setDate(String date) {
-        this.date = date;
+        this.date = date; //1
+        // o(1)
     }
 
     public void setTime(String time) {
-        this.time = time;
+        this.time = time; //1
+        //o(1)
     }
 
     public void setLocation(String location) {
-        this.location = location;
+        this.location = location; //1
+        // o(1)
     }
 
 //    public void setContact(String contact) {
-//        this.contact = contact;
+//        this.contact = contact;  
 //    }
 
     public String getTitle() {
-        return title;
+        return title; //1
+        //o(1)
     }
 
     public String getDate() {
-        return date;
+        return date; //1
+        // o(1)
     }
 
     public String getTime() {
-        return time;
+        return time; //1
+        // o(1)
     }
 
     public String getLocation() {
-        return location;
+        return location; //1
+        // o(1)
     }
 
     public String getContact() {
-        return contact.toString();
+        return contact.toString(); //1
+        // o(1)
     }
 
     @Override
